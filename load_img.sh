361cc0a2908c6535f688a494d7b3643a5549fed7
Olezhich <qwertypx@yandex.ru>
2026-02-22 15:06:22 +0300

    image loading filter added

---
+++ load_img.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DIR=$(pwd)
+IFS=$'\t' read -ra f
+
+curl -so "$DIR/thumb.jpg" "${f[7]}"
+curl -so "$DIR/cover_tmp.jpg" "${f[8]}"
+
+s=${f[9]}; t=${f[10]}; m=$s; (( t < s )) && m=$t
+
+convert "$DIR/cover_tmp.jpg" -gravity center -crop ${m}x${m}+0+0 +repage "$DIR/cover.jpg" 2>/dev/null
+
+rm "$DIR/cover_tmp.jpg"
+
+
+
+printf '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' "${f[@]:0:7}" "$DIR/thumb.jpg" "$DIR/cover.jpg"
+
+while IFS=$'\t' read -r -a f; do
+    printf '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' "${f[@]:0:7}" "$DIR/thumb.jpg" "$DIR/cover.jpg"
+done
