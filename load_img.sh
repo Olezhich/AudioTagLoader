8e7b47f217093159b8166621f06adff21be3444c
Olezhich <qwertypx@yandex.ru>
2026-02-23 13:53:13 +0300

    load_img sh columns order fixed

--- load_img.sh
+++ load_img.sh
@@ -8,7 +8,7 @@ IFS=$'\t' read -ra f
 curl -so "$DIR/thumb.jpg" "${f[6]}"
 curl -so "$DIR/cover_tmp.jpg" "${f[7]}"
 
-s=${f[9]}; t=${f[10]}; m=$s; (( t < s )) && m=$t
+s=${f[8]}; t=${f[9]}; m=$s; (( t < s )) && m=$t
 
 convert "$DIR/cover_tmp.jpg" -gravity center -crop ${m}x${m}+0+0 +repage "$DIR/cover.jpg" 2>/dev/null
 
